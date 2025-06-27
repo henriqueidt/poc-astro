008dc3bc3e196c78136166952b3330fbe937af8f
Henrique Eidt <henriqueidt@gmail.com>
2025-06-26 23:30:14 -0300

    Add framework component with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://poc-astro-eidt.netlify.app/",
+  integrations: [preact()],
 });
